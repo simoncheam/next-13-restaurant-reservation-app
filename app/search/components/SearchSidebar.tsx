6686ee6f4cd363a92ed01c16ea599bbee26de315
Simon <81940817+simoncheam@users.noreply.github.com>
2023-07-29 19:25:55 -0400

    filtering by query params is  working

--- app/search/components/SearchSidebar.tsx
+++ app/search/components/SearchSidebar.tsx
@@ -1,31 +1,116 @@
-const SearchSidebar = () => {
+import { Cuisine, Location, PRICE } from '@prisma/client';
+import Link from 'next/link';
+import Price from '../../components/Price';
+
+const SearchSidebar = ({
+  locations,
+  cuisines,
+  searchParams,
+}: {
+  locations: Location[];
+  cuisines: Cuisine[];
+  searchParams: { city?: string | undefined; cuisine?: string | undefined; price?: string | undefined };
+}) => {
+  // create DRY price array
+  const prices = [
+    { price: PRICE.CHEAP, label: '$',
+    className: 'border w-full text-reg text-center font-light rounded-l p2' },
+    { price: PRICE.REGULAR, label: '$$',
+    className: 'border w-full text-reg text-center font-light p2' },
+    { price: PRICE.EXPENSIVE, label: '$$$',
+    className: 'border w-full text-reg text-center font-light rounded-r p2' },
+  ];
+
   return (
     <div className='w-1/5'>
-    <div className='border-b pb-4'>
-      <h1>Regions</h1>
-      <p className='font-light text-reg'>Toronto</p>
-      <p className='font-light text-reg'>Ottawa</p>
-      <p className='font-light text-reg'>Montreal</p>
-      <p className='font-light text-reg'>Hamilton</p>
-      <p className='font-light text-reg'>Kingston</p>
-      <p className='font-light text-reg'>Niagara</p>
-    </div>
-    <div className='border-b pb-4'>
-      <h1>Cuisine</h1>
-      <p className='font-light text-reg'>Mexican</p>
-      <p className='font-light text-reg'>Indian</p>
-      <p className='font-light text-reg'>Italian</p>
-      <p className='font-light text-reg'>Japanese</p>
-    </div>
-    <div className='mt-3 pb-4'>
-      <h1 className='mb-2'>Price</h1>
-      <div className='flex'>
-        <button className='border w-full text-reg font-light rounded-l p2'>$</button>
-        <button className='border-r border-t border-b w-full text-reg font-light  p2'>$$</button>
-        <button className='border-r border-t border-b w-full text-reg font-light rounded-r p2'>$$$</button>
+      <div className='border-b pb-4 flex flex-col'>
+        <h1>Regions</h1>
+        {locations.map((location) => (
+          // ! use link to add query params to url
+          <Link
+            href={{
+              pathname: '/search',
+              query: {
+                ...searchParams, // destructuring searchParams
+                city: location.name,
+              },
+            }}
+            key={location.id}
+            className='font-light text-reg capitalize'>
+            {location.name}
+          </Link>
+        ))}
+      </div>
+      <div className='border-b pb-4 flex flex-col'>
+        <h1>Cuisine</h1>
+        {cuisines.map((cuisine) => (
+          <Link
+            href={{
+              pathname: '/search',
+              query: {
+                ...searchParams, // destructuring searchParams
+                cuisine: cuisine.name,
+              },
+            }}
+            key={cuisine.id}
+            className='font-light text-reg capitalize'>
+            {cuisine.name}
+          </Link>
+        ))}
+      </div>
+      <div className='mt-3 pb-4'>
+        <h1 className='mb-2'>Price</h1>
+        <div className='flex'>
+          {prices.map(({ price, label, className }) => (
+            <Link
+              href={{
+                pathname: '/search',
+                query: {
+                  ...searchParams, // destructuring searchParams
+                  price: price, // destructuring
+                },
+              }}
+              className={className}>
+              {label}
+            </Link>
+          ))}
+
+          {/* <Link
+            href={{
+              pathname: '/search',
+              query: {
+                ...searchParams, // destructuring searchParams
+                price: PRICE.CHEAP,
+              },
+            }}
+            className='border w-full text-reg font-light rounded-l p2'>
+            $
+          </Link>
+          <Link
+            href={{
+              pathname: '/search',
+              query: {
+                ...searchParams, // destructuring searchParams
+                price: PRICE.REGULAR,
+              },
+            }}
+            className='border-r border-t border-b w-full text-reg font-light  p2'>
+            $$
+          </Link>
+          <Link
+            href={{
+              pathname: '/search',
+              query: {
+                ...searchParams, // destructuring searchParams
+                price: PRICE.EXPENSIVE,
+              },
+            }}
+            className='border-r border-t border-b w-full text-reg font-light rounded-r p2'>
+            $$$
+          </Link> */}
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
-export default SearchSidebar
+  );
+};
+export default SearchSidebar;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PRICE, PrismaClient } from '@prisma/client';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 import SearchSidebar from './components/SearchSidebar';
@@ -15,38 +15,87 @@ const select = {
   slug: true,
 };
 
-const fetchRestaurantsByCity = (city: string | undefined) => {
-  if (!city) {
-    return prisma.restaurant.findMany({ select });
+interface SearchParams {
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
+
+// * create where object for prisma query (where)
+interface whereParams {
+  location?: { name: { equals: string } };
+  cuisine?: { name: { equals: string } };
+  price?: { equals: PRICE };
+}
+
+const fetchRestaurantsBySearchParams = (searchParams: SearchParams) => {
+  // ! construct where object based on searchParams
+  const where: whereParams = {};
+
+  // ! checks for city, cuisine, price
+  if (searchParams.city) {
+    const location = {
+      name: {
+        equals: searchParams.city.toLowerCase(),
+      },
+    };
+    // * add location property to where object
+    where.location = location;
   }
+  if (searchParams.cuisine) {
+    const cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase(),
+      },
+    };
+
+    where.cuisine = cuisine;
+  }
+  if (searchParams.price) {
+    const price = {
+      equals: searchParams.price,
+    };
+
+    where.price = price;
+  }
+  console.log({ where })
 
   return prisma.restaurant.findMany({
-    where: {
-      location: {
-        name: {
-          equals: city.toLowerCase(),
-        },
-      },
-    },
+    where,
     select,
   });
 };
 
-const Search = async ({ searchParams }: { searchParams: { city: string } }) => {
-  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+//fetch locations
+
+const fetchLocations = async () => {
+  return prisma.location.findMany();
+};
+
+//fetch cuisines
+const fetchCuisines = async () => {
+  return prisma.cuisine.findMany();
+};
+
+const Search = async ({ searchParams }: { searchParams: { city?: string; cuisine?: string; price?: PRICE } }) => {
+  const restaurants = await fetchRestaurantsBySearchParams(searchParams);
+
+  // ! get all locations and cuisines
+  // ! pass them to SearchSidebar
+  const location = await fetchLocations();
+  const cuisine = await fetchCuisines();
 
   return (
     <>
       <Header />
       <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
-        <SearchSidebar />
+        <SearchSidebar locations={location} cuisines={cuisine} searchParams={searchParams} />
 
-        {/* if restaurants found show map of all restaurants, else show "no restaurants found" */}
+        {/* //! if restaurants found show map of all restaurants, else show "no restaurants found" */}
 
         <div className='w-5/6 '>
           {restaurants.length ? (
-            restaurants.map((restaurant) =>
-            <RestaurantCard key={restaurant.id} restaurant={restaurant}/>)
+            restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)
           ) : (
             <div className='w-5/6'>No restaurants found in " {searchParams.city}"</div>
           )}
